ed47f1939cb7266b776fc4ec25ed391ba45671c3
marcelo <marceloventura@outlook.com>
2018-08-11 18:09:46 -0700

    Enemy Animator Controller + UI + Levels

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;      //Allows us to use SceneManager
 
@@ -9,13 +10,15 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
         public int pointsPerFood = 10;              //Number of points to add to player food points when picking up a food object.
         public int pointsPerSoda = 20;              //Number of points to add to player food points when picking up a soda object.
         public int wallDamage = 1;                  //How much damage a player does to a wall when chopping it.
+        public Text foodText;
         
         
         private Animator animator;                  //Used to store a reference to the Player's animator component.
         private int food;                           //Used to store player food points total during level.
         
-        
+        //---------------------------------------------------------------------------------------------------
         //Start overrides the Start function of MovingObject
+        //---------------------------------------------------------------------------------------------------
         protected override void Start ()
         {
             //Get a component reference to the Player's animator component
@@ -23,20 +26,25 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
             
             //Get the current food point total stored in GameManager.instance between levels.
             food = GameManager.instance.playerFoodPoints;
+
+            foodText.text = "Food = " + food;
             
             //Call the Start function of the MovingObject base class.
             base.Start ();
         }
         
-        
+        //---------------------------------------------------------------------------------------------------
         //This function is called when the behaviour becomes disabled or inactive.
+        //---------------------------------------------------------------------------------------------------
         private void OnDisable ()
         {
             //When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
             GameManager.instance.playerFoodPoints = food;
         }
         
-        
+        //---------------------------------------------------------------------------------------------------
+        //
+        //---------------------------------------------------------------------------------------------------
         private void Update ()
         {
             //If it's not the player's turn, exit the function.
@@ -66,14 +74,17 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
                 AttemptMove<Wall> (horizontal, vertical);
             }
         }
-        
+        //---------------------------------------------------------------------------------------------------
         //AttemptMove overrides the AttemptMove function in the base class MovingObject
         //AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
+        //---------------------------------------------------------------------------------------------------
         protected override void AttemptMove <T> (int xDir, int yDir)
         {
             //Every time player moves, subtract from food points total.
             food--;
             
+            foodText.text = "Food = " + food;
+
             //Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
             base.AttemptMove <T> (xDir, yDir);
             
@@ -93,9 +104,10 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
             GameManager.instance.playersTurn = false;
         }
         
-        
+        //---------------------------------------------------------------------------------------------------
         //OnCantMove overrides the abstract function OnCantMove in MovingObject.
         //It takes a generic parameter T which in the case of Player is a Wall which the player can attack and destroy.
+        //---------------------------------------------------------------------------------------------------
         protected override void OnCantMove <T> (T component)
         {
             //Set hitWall to equal the component passed in as a parameter.
@@ -108,8 +120,9 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
             animator.SetTrigger ("playerChop");
         }
         
-        
+        //---------------------------------------------------------------------------------------------------
         //OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+        //---------------------------------------------------------------------------------------------------
         private void OnTriggerEnter2D (Collider2D other)
         {
             //Check if the tag of the trigger collided with is Exit.
@@ -127,6 +140,8 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
             {
                 //Add pointsPerFood to the players current food total.
                 food += pointsPerFood;
+                //
+                foodText.text = "+" + pointsPerFood + " Food = " + food;
                 
                 //Disable the food object the player collided with.
                 other.gameObject.SetActive (false);
@@ -137,7 +152,8 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
             {
                 //Add pointsPerSoda to players food points total
                 food += pointsPerSoda;
-                
+                //
+                foodText.text = "+" + pointsPerSoda + " Food = " + food;
                 
                 //Disable the soda object the player collided with.
                 other.gameObject.SetActive (false);
@@ -162,7 +178,8 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
             
             //Subtract lost food points from the players total.
             food -= loss;
-            
+            //
+            foodText.text = "- " + loss + " Food = " + food;
             //Check to see if game has ended.
             CheckIfGameOver ();
         }
