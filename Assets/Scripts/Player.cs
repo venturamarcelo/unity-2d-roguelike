4a57f6befa2dac79d7dbf7b90312c33c9e6552da
marcelo <marceloventura@outlook.com>
2018-08-12 12:02:43 -0700

    Adding Mobile Controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,17 +11,18 @@ using UnityEngine.SceneManagement;                  //Allows us to use SceneMana
         public int wallDamage = 1;                  //How much damage a player does to a wall when chopping it.
         public Text foodText;
 
-        public AudioClip moveSound1;
-        public AudioClip moveSound2;
-        public AudioClip eatSound1;
-        public AudioClip eatSound2;
-        public AudioClip drinkSound1;
-        public AudioClip drinkSound2;
-        public AudioClip gameOverSound;
+        public AudioClip moveSound1;                //1 of 2 Audio clips to play when player moves.
+        public AudioClip moveSound2;                //2 of 2 Audio clips to play when player moves.
+        public AudioClip eatSound1;                 //1 of 2 Audio clips to play when player collects a food object.
+        public AudioClip eatSound2;                 //2 of 2 Audio clips to play when player collects a food object.
+        public AudioClip drinkSound1;               //1 of 2 Audio clips to play when player collects a soda object.
+        public AudioClip drinkSound2;               //2 of 2 Audio clips to play when player collects a soda object.
+        public AudioClip gameOverSound;             //Audio clip to play when player dies.
         
         
         private Animator animator;                  //Used to store a reference to the Player's animator component.
         private int food;                           //Used to store player food points total during level.
+        private Vector2 touchOrigin = -Vector2.one; //Used to store location of screen touch origin for mobile controls.
         
         //---------------------------------------------------------------------------------------------------
         //Start overrides the Start function of MovingObject
@@ -61,6 +62,9 @@ using UnityEngine.SceneManagement;                  //Allows us to use SceneMana
             int vertical = 0;       //Used to store the vertical move direction.
             
             
+            //Check if we are running either in the Unity editor or in a standalone build.
+            #if UNITY_STANDALONE || UNITY_WEBPLAYER
+            
             //Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
             horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
             
@@ -68,9 +72,52 @@ using UnityEngine.SceneManagement;                  //Allows us to use SceneMana
             vertical = (int) (Input.GetAxisRaw ("Vertical"));
             
             //Check if moving horizontally, if so set vertical to zero.
-            if(horizontal != 0){
+            if(horizontal != 0) {
+
                 vertical = 0;
             }
+            //Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+            #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+            
+            //Check if Input has registered more than zero touches
+            if (Input.touchCount > 0) {
+
+                //Store the first touch detected.
+                Touch myTouch = Input.touches[0];
+                
+                //Check if the phase of that touch equals Began
+                if (myTouch.phase == TouchPhase.Began) {
+
+                    //If so, set touchOrigin to the position of that touch
+                    touchOrigin = myTouch.position;
+                }
+                
+                //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+                else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0){
+
+                    //Set touchEnd to equal the position of this touch
+                    Vector2 touchEnd = myTouch.position;
+                    
+                    //Calculate the difference between the beginning and end of the touch on the x axis.
+                    float x = touchEnd.x - touchOrigin.x;
+                    
+                    //Calculate the difference between the beginning and end of the touch on the y axis.
+                    float y = touchEnd.y - touchOrigin.y;
+                    
+                    //Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+                    touchOrigin.x = -1;
+                    
+                    //Check if the difference along the x axis is greater than the difference along the y axis.
+                    if (Mathf.Abs(x) > Mathf.Abs(y))
+                        //If x is greater than zero, set horizontal to 1, otherwise set it to -1
+                        horizontal = x > 0 ? 1 : -1;
+                    else
+                        //If y is greater than zero, set horizontal to 1, otherwise set it to -1
+                        vertical = y > 0 ? 1 : -1;
+                }
+            }
+            
+            #endif //End of mobile platform dependendent compilation section started above with #elif
             
             //Check if we have a non-zero value for horizontal or vertical
             if(horizontal != 0 || vertical != 0){
